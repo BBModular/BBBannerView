b9c1ead4f519c99e04cc15e3ef6e0d0b622336f5
吴云龙 <yunlong.wu@bblink.cn>
2017-05-10 14:22:36 +0800

    新建项目

---
+++ BBBannerView/BBBannerView.h
@@ -0,0 +1,19 @@
+//
+//  BBBannerView.h
+//  BBBannerView
+//
+//  Created by yunlongMac on 2017/5/10.
+//  Copyright © 2017年 lvjesus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBBannerView.
+FOUNDATION_EXPORT double BBBannerViewVersionNumber;
+
+//! Project version string for BBBannerView.
+FOUNDATION_EXPORT const unsigned char BBBannerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBBannerView/PublicHeader.h>
+
+
---
+++ BBBannerViewDemo/BBBannerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBBannerViewDemo
+//
+//  Created by yunlongMac on 2017/5/10.
+//  Copyright © 2017年 lvjesus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
